0a432d02f127cc09e9ba903aa9ce47d65e2e8987
charlie wanek <charlie@victorops.com>
2014-05-26 12:21:43 -0600

    add a simple backfill

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,6 +1,7 @@
 
 import java.io.File
 import scala.io.Source
+import scala.concurrent.duration._
 import com.twitter.hbc.ClientBuilder
 import com.twitter.hbc.core._
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
@@ -12,17 +13,42 @@ import org.apache.commons.lang3.StringEscapeUtils
 import com.twitter.hbc.twitter4j._
 import com.twitter.hbc.twitter4j.message._
 import twitter4j._
+import twitter4j.auth._
 import java.util.concurrent._
 
+// args: <consumerkey> <consumersecret> <token> <tokensecret> [<run mins>]
 object Test extends App {
+
+  val lastIDFilename = "/tmp/twitter.lastid.txt"
+  val follow: List[java.lang.Long] = List(1344951,5988062,807095,3108351,3243351,20245577,16906137)
+  var lastStatusID = loadLastStatusID
+
+  def loadLastStatusID = {
+    if ( new File(lastIDFilename).exists )
+      Source.fromFile(lastIDFilename).getLines.next.toLong
+    else
+      0L
+  }
+
+  def updateLastStatusID( status: Status ) {
+    if ( status.getId > lastStatusID ) {
+      val p = new java.io.PrintWriter(lastIDFilename)
+      try { p.println(status.getId) } finally { p.close() }
+      lastStatusID = status.getId
+    }
+  }
+
+  def dispStatus( status: Status ) {
+    if ( !status.isRetweet )
+      println(s"""[${status.getId}]${status.getUser.getName}: ${status.getText}""")
+    else
+      print(".")
+    updateLastStatusID(status)
+  }
 
   def simpleStatusListener = new com.twitter.hbc.twitter4j.handler.StatusStreamHandler () {
     def onStatus(status: Status) {
-      if ( !status.isRetweet )
-        println(s"""
-          |${status.getUser.getName}: ${status.getText}""".stripMargin)
-      else
-        print(".")
+      dispStatus(status)
     }
 
     def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice) { println("onDeletionNotice") }
@@ -51,34 +77,45 @@ object Test extends App {
     def onUnknownMessageType(msg: String){ println("onUnknownMessageType") }
   }
 
-
-  override def main(args: Array[String]) {
-    import scala.concurrent.duration._
-
-    // args: <consumerkey> <consumersecret> <token> <tokensecret> [<run mins>]
-    val oauth = new OAuth1(args(0),args(1),args(2),args(3))
-    val runTime = (if (args.length>4) args(4).toInt else 1).minutes
-
-    val msgQueue = new LinkedBlockingQueue[String](100000)
-    val follow: List[java.lang.Long] = List(1344951,5988062,807095,3108351,3243351,20245577,16906137)
-    val endpoint = new StatusesFilterEndpoint().followings(follow.asJava)
-
-    val client = new ClientBuilder()
-              .hosts(Constants.USERSTREAM_HOST)
-              .authentication(oauth)
-              .processor(new StringDelimitedProcessor(msgQueue))
-              .endpoint(endpoint)
-              .build()
-
-    val t4jClient = new Twitter4jStatusClient( client, msgQueue, List(simpleStatusListener).asJava, Executors.newFixedThreadPool(5) )
-    t4jClient.connect()
-    t4jClient.process()
-
-    println(s"Running for $runTime...")
-
-    Thread.sleep(runTime.toMillis)
-
-    println("Stopping...")
-    t4jClient.stop
+  def backfill( twitter: Twitter, userID: Long, since: Long ) {
+    val paging = new Paging(since)
+    for ( status <- twitter.getUserTimeline(userID, paging).asScala.reverse ) {
+      dispStatus(status)
+    }
   }
+
+  val twitter: Twitter = TwitterFactory.getSingleton
+  twitter.setOAuthConsumer(args(0),args(1))
+  twitter.setOAuthAccessToken(new AccessToken(args(2),args(3)))
+
+  if ( lastStatusID > 0 ) {
+    println(s"backfilling from $lastStatusID")
+    val minID = lastStatusID
+    for ( id <- follow ) {
+      backfill(twitter, id, minID)
+    }
+  }
+
+  val oauth = new OAuth1(args(0),args(1),args(2),args(3))
+  val runTime = (if (args.length>4) args(4).toInt else 1).minutes
+
+  val msgQueue = new LinkedBlockingQueue[String](100000)
+  val endpoint = new StatusesFilterEndpoint().followings(follow.asJava)
+
+  val client = new ClientBuilder()
+            .hosts(Constants.USERSTREAM_HOST)
+            .authentication(oauth)
+            .processor(new StringDelimitedProcessor(msgQueue))
+            .endpoint(endpoint)
+            .build()
+
+  println(s"Running for $runTime...")
+  val t4jClient = new Twitter4jStatusClient( client, msgQueue, List(simpleStatusListener).asJava, Executors.newFixedThreadPool(5) )
+  t4jClient.connect()
+  t4jClient.process()
+
+  Thread.sleep(runTime.toMillis)
+
+  println("Stopping...")
+  t4jClient.stop
 }
